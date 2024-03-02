a60d3ac61c6dc97cc018ce98f976b0a2772a65e0
dipendra-kumar <bhattdipak356@gmail.com>
2024-03-02 10:08:48 -0800

    changes

--- components/HeadTitle.tsx
+++ components/HeadTitle.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 
 import { Nunito_Sans } from "next/font/google";
-import { fadeIn, textVariant } from "@/utils/motion";
+import { fadeIn, textVariant, zoomIn } from "@/utils/motion";
 const nunitoSans = Nunito_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
@@ -16,7 +16,7 @@ const HeadTitle = ({ title }: TitleProps) => {
   return (
     <div className="w-full flex items-center justify-center gap-5 my-10">
       <motion.div
-        variants={fadeIn("right", "tween", 0.2, 0.5)}
+        variants={zoomIn(0.2, 0.5)}
         className="w-20 bg-blue-400 h-2 rounded-full"
       ></motion.div>
       <motion.h2
@@ -26,7 +26,7 @@ const HeadTitle = ({ title }: TitleProps) => {
         {title}
       </motion.h2>
       <motion.div
-        variants={fadeIn("left", "tween", 0.2, 0.5)}
+        variants={zoomIn(0.2, 0.5)}
         className="w-20 bg-blue-400 h-2 rounded-full"
       ></motion.div>
     </div>
