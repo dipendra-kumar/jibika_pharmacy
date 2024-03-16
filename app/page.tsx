b5cef409c07e455f6f006ecc6a9742028069504d
dipendra-kumar <bhattdipak356@gmail.com>
2024-03-16 13:23:23 -0700

    build fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,6 @@
-"use client";
-import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React from "react";
 
 const page = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    document.title = "Jibika Pharmacy & Health Clinic - Home";
-    router.push("/home");
-  }, []);
   return <div></div>;
 };
 
