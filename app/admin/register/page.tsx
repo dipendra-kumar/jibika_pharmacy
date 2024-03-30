1f660718224c06be852f26793fde70d1f4a1ecfe
dipendra-kumar <bhattdipak356@gmail.com>
2024-03-30 22:47:12 +0545

    some fixes

--- app/admin/register/page.tsx
+++ app/admin/register/page.tsx
@@ -7,7 +7,7 @@ import Loading from "@/components/Loading";
 
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
-import { registerAdmin } from "@/actions/auth";
+// import { registerAdmin } from "@/actions/auth";
 
 interface RegisterFormInput {
   fullName: string;
@@ -30,7 +30,7 @@ const Register: React.FC = () => {
   const onSubmit = async (data: RegisterFormInput) => {
     try {
       setIsLoading(true);
-      registerAdmin(data);
+      // registerAdmin(data);
       setIsLoading(false);
       toast({
         title: "Registration successful,",
@@ -48,8 +48,8 @@ const Register: React.FC = () => {
     }
   };
   return (
-    <div className="h-screen w-full bg-gray-100 flex flex-col justify-center py-12 sm:px-6 px-8">
-      <div className="flex flex-col gap-10 mx-auto px-8 py-12 bg-white shadow rounded-lg md:px-10 w-full md:min-w-[385px] md:max-w-[500px]">
+    <div className="flex h-screen w-full flex-col justify-center bg-gray-100 px-8 py-12 sm:px-6">
+      <div className="mx-auto flex w-full flex-col gap-10 rounded-lg bg-white px-8 py-12 shadow md:min-w-[385px] md:max-w-[500px] md:px-10">
         <h2 className="text-center text-3xl font-extrabold text-gray-900">
           Jibika Pharmacy <br />& Health Clinic
         </h2>
@@ -102,8 +102,8 @@ const Register: React.FC = () => {
           <div>
             <Button
               type="submit"
-              className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${
-                isLoading ? "opacity-50 cursor-not-allowed" : ""
+              className={`flex w-full justify-center rounded-md border border-transparent bg-primary px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 ${
+                isLoading ? "cursor-not-allowed opacity-50" : ""
               }`}
               disabled={isLoading}
             >
