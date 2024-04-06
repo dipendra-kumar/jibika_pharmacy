264293d9224451fc0a62bd4a483d683ce322e2f5
Dipendra Kumar Bhatt <bhattdipak356@gmail.com>
2024-04-06 12:26:11 +0545

    fix:intrinsic attribs for loading component

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,6 +1,6 @@
 import { FaSpinner } from "react-icons/fa";
 
-const Loading: React.FC = ({ className }: { className?: string }) => {
+const Loading = ({ className }: { className?: string }) => {
   return (
     <div className="flex h-full w-full items-center justify-center">
       <FaSpinner className={`animate-spin  ${className} `} />
